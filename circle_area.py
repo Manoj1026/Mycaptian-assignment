956b9efd76b2d8586142c89f7a74fcec023a5f7c
Manoj Kumar <94540786+Manoj1026@users.noreply.github.com>
2022-04-10 18:36:03 +0530

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,6 @@
+r=float(input('Input the radius of the circle:'))
+a=4*3.1428*r*r
+print("area of the circle with the radius",r,"is: ",a)
+input("press [Enter] to continue.")
+
+
---
+++ file_extension.py
@@ -0,0 +1,6 @@
+a=input('input the file name: ')
+b=a.split(".")
+if (b[1] == "py"):
+    print("The extension of the file is: python")
+else:
+    print("extension of the file is not found")
